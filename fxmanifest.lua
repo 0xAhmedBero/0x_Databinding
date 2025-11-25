41a8445d99a388ebb26fa308f1958e52c03e728e
ahmed bero <ahmedbero543@gmail.com>
2025-11-25 16:35:14 +0100

    changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,9 @@ games {"rdr3"}
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-name '0xRedCore'
+name '0xDatabinding'
 author 'AhmedBero'
-description 'A Population script for RedCore'
-version '1.0.0'
+description 'Databinding Research'
 
 client_scripts {
     'client/BattlePass.lua',
